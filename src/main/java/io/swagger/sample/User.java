2f305fc3b1f3ec6bc34970fe0dfbda37d670f66e
Tony Tam <fehguy@gmail.com>
2015-07-18 23:40:53 -0700

    added test model

---
+++ src/main/java/io/swagger/sample/User.java
@@ -0,0 +1,20 @@
+package io.swagger.sample;
+
+public class User {
+  private Long id;
+  private String user;
+  
+  public Long getId() {
+    return id;
+  }
+  public void setId(Long id) {
+    this.id = id;
+  }
+  
+  public String getUser() {
+    return user;
+  }
+  public void setUser(String user) {
+    this.user = user;
+  } 
+}
