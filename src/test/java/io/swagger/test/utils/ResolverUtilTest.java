442979e08baa505d39f0a3f7f69cc8ac8f3a0493
Tony Tam <fehguy@gmail.com>
2016-02-10 21:07:06 -0800

    added controller, tests for #78

--- src/test/java/io/swagger/test/utils/ResolverUtilTest.java
+++ src/test/java/io/swagger/test/utils/ResolverUtilTest.java
@@ -1,5 +1,8 @@
 package io.swagger.test.utils;
 
+import com.fasterxml.jackson.databind.JavaType;
+import io.swagger.inflector.config.Configuration;
+import io.swagger.inflector.utils.ReflectionUtils;
 import io.swagger.inflector.utils.ResolverUtil;
 import io.swagger.models.ArrayModel;
 import io.swagger.models.Model;
@@ -9,9 +12,11 @@ import io.swagger.models.parameters.BodyParameter;
 import io.swagger.models.parameters.Parameter;
 import io.swagger.models.properties.ObjectProperty;
 import io.swagger.parser.SwaggerParser;
+import io.swagger.sample.models.Dog;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertTrue;
+import static org.testng.AssertJUnit.assertEquals;
 
 public class ResolverUtilTest {
     @Test
@@ -27,5 +32,20 @@ public class ResolverUtilTest {
         Model model = body.getSchema();
         assertTrue(model instanceof ArrayModel);
         assertTrue(((ArrayModel)model).getItems() instanceof ObjectProperty);
+    }
+
+    @Test
+    public void testResolveBodyParam() throws Exception {
+        ReflectionUtils utils = new ReflectionUtils();
+        utils.setConfiguration( Configuration.read("src/test/config/config1.yaml"));
+
+        Swagger swagger = new SwaggerParser().read("./src/test/swagger/sample1.yaml");
+        new ResolverUtil().resolveFully(swagger);
+
+        Operation operation = swagger.getPath("/mappedWithDefinedModel/{id}").getPost();
+        Parameter param = operation.getParameters().get(1);
+
+        JavaType jt = utils.getTypeFromParameter(param, swagger.getDefinitions());
+        assertEquals(jt.getRawClass(), Dog.class);
     }
 }
