77ddb4c722d3ab4fdb7e4041294f901b97db345b
gracekarina <gracekarina.gonzalez@gmail.com>
2017-11-10 18:28:45 -0500

    refactoring validators number and string

--- src/main/java/io/swagger/oas/inflector/validators/NumericValidator.java
+++ src/main/java/io/swagger/oas/inflector/validators/NumericValidator.java
@@ -2,6 +2,7 @@ package io.swagger.oas.inflector.validators;
 
 
 import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.oas.models.parameters.RequestBody;
 
@@ -13,32 +14,28 @@ import java.util.Set;
 
 public class NumericValidator implements Validator {
     public void validate(Object argument, Parameter parameter, Iterator<Validator> chain) throws ValidationException {
+
         if(argument != null && parameter.getSchema() != null) {
-            if(parameter.getSchema().getEnum() != null && parameter.getSchema().getEnum().size() > 0) {
-                List<?> values = parameter.getSchema().getEnum();
-                Set<String> allowable = new LinkedHashSet<String>();
-                for(Object obj : values) {
-                    allowable.add(obj.toString());
-                }
-                if(!allowable.contains(argument.toString())) {
-                    throw new ValidationException()
-                        .message(new ValidationMessage()
+
+            Set<String> allowable = validateEnum(argument, parameter.getSchema());
+            if(allowable != null){
+                throw new ValidationException()
+                    .message(new ValidationMessage()
                             .code(ValidationError.UNACCEPTABLE_VALUE)
                             .message(parameter.getIn() + " parameter `" + parameter.getName() + " value `" + argument + "` is not in the allowable values `" + allowable + "`"));
-                }
-            };
+            }
+
+
+
             if(parameter.getSchema().getMaximum() != null) {
-                double max = parameter.getSchema().getMaximum().doubleValue();
-                Double value;
-                try {
-                    value = Double.parseDouble(argument.toString());
-                }
-                catch (NumberFormatException e) {
+                Double value = validateFormat(argument,parameter.getSchema());
+                if(value == null){
                     throw new ValidationException()
-                            .message(new ValidationMessage()
-                                    .code(ValidationError.INVALID_FORMAT)
-                                    .message(parameter.getIn() + " parameter `" + parameter.getName() + " is not a compatible number"));
+                        .message(new ValidationMessage()
+                                .code(ValidationError.INVALID_FORMAT)
+                                .message(parameter.getIn() + " parameter `" + parameter.getName() + " is not a compatible number"));
                 }
+                double max = parameter.getSchema().getMaximum().doubleValue();
                 if(parameter.getSchema().getExclusiveMaximum() != null && parameter.getSchema().getExclusiveMaximum()) {
                     if(value >= max) {
                         throw new ValidationException()
@@ -56,18 +53,17 @@ public class NumericValidator implements Validator {
                     }
                 }
             }
+
             if(parameter.getSchema().getMinimum() != null) {
                 double min = parameter.getSchema().getMinimum().doubleValue();
-                Double value;
-                try {
-                    value = Double.parseDouble(argument.toString());
-                }
-                catch (NumberFormatException e) {
+                Double value = validateFormat(argument,parameter.getSchema());
+                if(value == null){
                     throw new ValidationException()
                             .message(new ValidationMessage()
                                     .code(ValidationError.INVALID_FORMAT)
                                     .message(parameter.getIn() + " parameter `" + parameter.getName() + " is not a compatible number"));
                 }
+
                 if(parameter.getSchema().getExclusiveMinimum() != null && parameter.getSchema().getExclusiveMinimum()) {
                     if(value <= min) {
                         throw new ValidationException()
@@ -95,81 +91,106 @@ public class NumericValidator implements Validator {
         return;
     }
 
+
+
+
+
+
+    private Double validateFormat(Object argument, Schema schema) {
+
+        Double value;
+        try {
+            value = Double.parseDouble(argument.toString());
+        }
+        catch (NumberFormatException e) {
+            return null;
+        }
+
+        return value;
+    }
+
+    public Set<String> validateEnum(Object argument, Schema schema){
+        if(schema.getEnum() != null && schema.getEnum().size() > 0) {
+            List<?> values = schema.getEnum();
+            Set<String> allowable = new LinkedHashSet<String>();
+            for(Object obj : values) {
+                allowable.add(obj.toString());
+            }
+            if(!allowable.contains(argument.toString())) {
+                return allowable;
+            }
+        }
+        return null;
+    }
+
     public void validate(Object argument, RequestBody body, Iterator<Validator> chain) throws ValidationException {
         if (body.getContent() != null) {
             for(String media: body.getContent().keySet()) {
                 if (body.getContent().get(media) != null) {
                     MediaType mediaType = body.getContent().get(media);
                     if (argument != null && mediaType.getSchema() != null) {
-                        if(mediaType.getSchema().getEnum() != null && mediaType.getSchema().getEnum().size() > 0) {
-                            List<?> values = mediaType.getSchema().getEnum();
-                            Set<String> allowable = new LinkedHashSet<String>();
-                            for(Object obj : values) {
-                                allowable.add(obj.toString());
-                            }
-                            if(!allowable.contains(argument.toString())) {
+                        Set<String> allowable = validateEnum(argument,mediaType.getSchema());
+                        if (allowable != null){
+                            throw new ValidationException()
+                                .message(new ValidationMessage()
+                                        .code(ValidationError.UNACCEPTABLE_VALUE)
+                                        .message( " parameter  value `" + argument + "` is not in the allowable values `" + allowable + "`"));
+
+                        }
+                        if(mediaType.getSchema().getMaximum() != null) {
+                            Double value = validateFormat(argument,mediaType.getSchema());
+                            if(value == null){
                                 throw new ValidationException()
                                         .message(new ValidationMessage()
-                                                .code(ValidationError.UNACCEPTABLE_VALUE)
-                                                .message( " parameter  value `" + argument + "` is not in the allowable values `" + allowable + "`"));
+                                                .code(ValidationError.INVALID_FORMAT)
+                                                .message(" parameter `" + body + " is not a compatible number"));
                             }
-                        };
-
-                        if (mediaType.getSchema().getMaximum() != null) {
                             double max = mediaType.getSchema().getMaximum().doubleValue();
-                            Double value;
-                            try {
-                                value = Double.parseDouble(argument.toString());
-                            } catch (NumberFormatException e) {
-                                throw new ValidationException()
-                                        .message(new ValidationMessage()
-                                                .code(ValidationError.INVALID_FORMAT)
-                                                .message(" parameter `" +  " is not a compatible number"));
-                            }
-                            if (mediaType.getSchema().getExclusiveMaximum() != null && mediaType.getSchema().getExclusiveMaximum()) {
-                                if (value >= max) {
+                            if(mediaType.getSchema().getExclusiveMaximum() != null && mediaType.getSchema().getExclusiveMaximum()) {
+                                if(value >= max) {
                                     throw new ValidationException()
                                             .message(new ValidationMessage()
                                                     .code(ValidationError.VALUE_OVER_MAXIMUM)
-                                                    .message(" parameter `"  + " value `" + argument + "` is greater than maximum allowed value `" + max + "`"));
+                                                    .message(" parameter `" + body + " value `" + argument + "` is greater than maximum allowed value `" + max + "`"));
                                 }
-                            } else {
-                                if (value > max) {
+                            }
+                            else {
+                                if(value > max) {
                                     throw new ValidationException()
                                             .message(new ValidationMessage()
                                                     .code(ValidationError.VALUE_OVER_MAXIMUM)
-                                                    .message(" parameter `" + " value `" + argument + "` is greater or equal to maximum allowed value `" + max + "`"));
-                                }
-                            }
-                        }
-                        if (mediaType.getSchema().getMinimum() != null) {
-                            double min = mediaType.getSchema().getMinimum().doubleValue();
-                            Double value;
-                            try {
-                                value = Double.parseDouble(argument.toString());
-                            } catch (NumberFormatException e) {
-                                throw new ValidationException()
-                                        .message(new ValidationMessage()
-                                                .code(ValidationError.INVALID_FORMAT)
-                                                .message( " parameter `" + " is not a compatible number"));
-                            }
-                            if (mediaType.getSchema().getExclusiveMinimum() != null && mediaType.getSchema().getExclusiveMinimum()) {
-                                if (value <= min) {
-                                    throw new ValidationException()
-                                            .message(new ValidationMessage()
-                                                    .code(ValidationError.VALUE_UNDER_MINIMUM)
-                                                    .message(" parameter `" + " value `" + argument + "` is less than minimum allowed value `" + min + "`"));
-                                }
-                            } else {
-                                if (value < min) {
-                                    throw new ValidationException()
-                                            .message(new ValidationMessage()
-                                                    .code(ValidationError.VALUE_UNDER_MINIMUM)
-                                                    .message(" parameter `" + " value `" + argument + "` is less or equal to the minimum allowed value `" + min + "`"));
+                                                    .message(" parameter `" + body + " value `" + argument + "` is greater or equal to maximum allowed value `" + max + "`"));
                                 }
                             }
                         }
 
+                        if(mediaType.getSchema().getMinimum() != null) {
+                            double min = mediaType.getSchema().getMinimum().doubleValue();
+                            Double value = validateFormat(argument,mediaType.getSchema());
+                            if(value == null){
+                                throw new ValidationException()
+                                        .message(new ValidationMessage()
+                                                .code(ValidationError.INVALID_FORMAT)
+                                                .message( " parameter `" + body+ " is not a compatible number"));
+                            }
+
+                            if(mediaType.getSchema().getExclusiveMinimum() != null && mediaType.getSchema().getExclusiveMinimum()) {
+                                if(value <= min) {
+                                    throw new ValidationException()
+                                            .message(new ValidationMessage()
+                                                    .code(ValidationError.VALUE_UNDER_MINIMUM)
+                                                    .message(" parameter `" + body + " value `" + argument + "` is less than minimum allowed value `" + min + "`"));
+                                }
+                            }
+                            else {
+                                if(value < min) {
+                                    throw new ValidationException()
+                                            .message(new ValidationMessage()
+                                                    .code(ValidationError.VALUE_UNDER_MINIMUM)
+                                                    .message(" parameter `" + body + " value `" + argument + "` is less or equal to the minimum allowed value `" + min + "`"));
+                                }
+                            }
+                        }
                     }
                 }
             }
--- src/main/java/io/swagger/oas/inflector/validators/StringTypeValidator.java
+++ src/main/java/io/swagger/oas/inflector/validators/StringTypeValidator.java
@@ -2,6 +2,7 @@ package io.swagger.oas.inflector.validators;
 
 
 import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.oas.models.parameters.RequestBody;
 import org.joda.time.DateTime;
@@ -13,36 +14,24 @@ import java.util.List;
 import java.util.Set;
 
 public class StringTypeValidator implements Validator {
+
     public void validate(Object argument, Parameter parameter, Iterator<Validator> chain) throws ValidationException {
         if(argument != null && parameter.getSchema() != null ) {
-            if(parameter.getSchema().getEnum() != null && parameter.getSchema().getEnum().size() > 0) {
-                List<?> values = parameter.getSchema().getEnum();
-                Set<String> allowable = new LinkedHashSet<String>();
-                for(Object obj : values) {
-                    allowable.add(obj.toString());
-                }
-                if(!allowable.contains(argument.toString())) {
-                    throw new ValidationException()
-                        .message(new ValidationMessage()
+            Set<String> allowable = validateAllowedValues(argument, parameter.getSchema());
+            if(allowable!= null){
+                throw new ValidationException()
+                    .message(new ValidationMessage()
                             .code(ValidationError.UNACCEPTABLE_VALUE)
                             .message(parameter.getIn() + " parameter `" + parameter.getName() + "` value `" + argument + "` is not in the allowable values `" + allowable + "`"));
-                }
             }
 
-            if("string".equals(parameter.getSchema().getType()) && ("date".equals(parameter.getSchema().getFormat()) || "date-time".equals(parameter.getSchema().getFormat()))) {
-              if(argument instanceof DateTime) {
-                // TODO
-              }
-              else if(argument instanceof LocalDate) {
-                // TODO
-              }
-              else {
+            if(validateFormat(argument,parameter.getSchema())){
                 throw new ValidationException()
-                  .message(new ValidationMessage()
-                    .code(ValidationError.INVALID_FORMAT)
-                    .message(parameter.getIn() + " parameter `" + parameter.getName() + " value `" + argument + "` is not a valid " + parameter.getSchema().getFormat()));
-              }
+                    .message(new ValidationMessage()
+                            .code(ValidationError.INVALID_FORMAT)
+                            .message(parameter.getIn() + " parameter `" + parameter.getName() + " value `" + argument + "` is not a valid " + parameter.getSchema().getFormat()));
             }
+
         }
         if(chain.hasNext()) {
             chain.next().validate(argument, parameter, chain);
@@ -57,42 +46,62 @@ public class StringTypeValidator implements Validator {
             for(String media: body.getContent().keySet()) {
                 if (body.getContent().get(media) != null) {
                     MediaType mediaType = body.getContent().get(media);
-                    if (argument != null && mediaType.getSchema() != null) {
-                        if(mediaType.getSchema().getEnum() != null && mediaType.getSchema().getEnum().size() > 0) {
-                            List<?> values = mediaType.getSchema().getEnum();
-                            Set<String> allowable = new LinkedHashSet<String>();
-                            for(Object obj : values) {
-                                allowable.add(obj.toString());
-                            }
-                            if(!allowable.contains(argument.toString())) {
-                                throw new ValidationException()
-                                        .message(new ValidationMessage()
-                                                .code(ValidationError.UNACCEPTABLE_VALUE)
-                                                .message(" parameter  value `" + argument + "` is not in the allowable values `" + allowable + "`"));
-                            }
-                        };
-
-                        if ("string".equals(mediaType.getSchema().getType()) && ("date".equals(mediaType.getSchema().getFormat()) || "date-time".equals(mediaType.getSchema().getFormat()))) {
-                            if (argument instanceof DateTime) {
-                                // TODO
-                            } else if (argument instanceof LocalDate) {
-                                // TODO
-                            } else {
-                                throw new ValidationException()
-                                        .message(new ValidationMessage()
-                                                .code(ValidationError.INVALID_FORMAT)
-                                                .message( " parameter `" +  " value `" + argument + "` is not a valid " + mediaType.getSchema().getFormat()));
-                            }
-                        }
+                    Set<String> allowable = validateAllowedValues(argument, mediaType.getSchema());
+                    if(allowable != null){
+                        throw new ValidationException()
+                            .message(new ValidationMessage()
+                                    .code(ValidationError.UNACCEPTABLE_VALUE)
+                                    .message(" parameter  value `" + argument + "` is not in the allowable values `" + allowable + "`"));
                     }
+                    if (validateFormat(argument,mediaType.getSchema())){
+                        throw new ValidationException()
+                            .message(new ValidationMessage()
+                                    .code(ValidationError.INVALID_FORMAT)
+                                    .message( " parameter value `" + argument + "` is not a valid " + mediaType.getSchema().getFormat()));
+                    }
+
+
                 }
             }
         }
+
         if(chain.hasNext()) {
             chain.next().validate(argument, body, chain);
             return;
         }
 
         return;
+    }
+
+    public Set<String> validateAllowedValues(Object argument, Schema schema){
+        if (argument != null && schema != null) {
+            if (schema.getEnum() != null && schema.getEnum().size() > 0) {
+                List<?> values = schema.getEnum();
+                Set<String> allowable = new LinkedHashSet<>();
+                for (Object obj : values) {
+                    allowable.add(obj.toString());
+                }
+                if (!allowable.contains(argument.toString())) {
+                    return allowable;
+                }
+            }
+        }
+        return  null;
+    }
+
+    public boolean validateFormat(Object argument, Schema schema) throws ValidationException{
+        if (argument != null && schema != null) {
+            if ("string".equals(schema.getType()) && ("date".equals(schema.getFormat()) || "date-time".equals(schema.getFormat()))) {
+                if (argument instanceof DateTime) {
+                    // TODO
+                } else if (argument instanceof LocalDate) {
+                    // TODO
+                } else {
+                    return true;
+                }
+            }
+
+        }
+        return false;
     }
 }
