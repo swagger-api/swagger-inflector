3240064e3e41f2eb9021367cef9a6bf20021ed0b
Tony Tam <tony@eatbacon.org>
2019-11-18 07:45:29 -0800

    updated bin, script

--- scripts/editor.sh
+++ scripts/editor.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -jar editor/jetty-runner.jar --port 8000 editor/swagger-editor.war
+java -Dconfig=editor/inflector.yaml -jar editor/jetty-runner.jar --port 8000 editor/swagger-editor.war
