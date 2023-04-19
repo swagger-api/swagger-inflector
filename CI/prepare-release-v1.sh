f81056cfeef484bb784f23a1bf8d6b6b7d1237aa
frantuma <frantuma@yahoo.com>
2023-04-19 12:57:13 +0200

    fix CI replacement in files

--- CI/prepare-release-v1.sh
+++ CI/prepare-release-v1.sh
@@ -32,6 +32,9 @@ sc_find="<version>$SC_LAST_RELEASE"
 sc_replace="<version>$SC_VERSION"
 sed -i -e "s/$sc_find/$sc_replace/g" $CUR/README.md
 
+sc_find="<version>$SC_VERSION-SNAPSHOT<\/version>"
+sc_replace="<version>$SC_VERSION<\/version>"
+sed -i -e "s/$sc_find/$sc_replace/g" $CUR/scripts/pom.xml
 
 #####################
 ### build and test maven ###
